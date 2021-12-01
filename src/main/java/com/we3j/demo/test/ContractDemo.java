eae355899dafe7c8942e4a79a6c1b4def768b1a7
Nate River <jambestwick@126.com>
2021-12-02 00:31:08 +0800

    增加合约的一些工具

--- src/main/java/com/we3j/demo/test/ContractDemo.java
+++ src/main/java/com/we3j/demo/test/ContractDemo.java
@@ -1,10 +1,35 @@
 package com.we3j.demo.test;
 
+import org.bouncycastle.util.encoders.HexEncoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.web3j.abi.FunctionEncoder;
+import org.web3j.abi.TypeReference;
+import org.web3j.crypto.ContractUtils;
+import org.web3j.crypto.RawTransaction;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.*;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.utils.Collection;
+import org.web3j.utils.Convert;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+
 /**
  * @auth jambestwick
- * ***/
+ ***/
 
 public class ContractDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ContractDemo.class);
+    private Web3j web3j;
+
     public static void main(String[] args) {
         new ContractDemo().run();
     }
@@ -12,5 +37,117 @@ public class ContractDemo {
     private void run() {
 
     }
+
+
+    /*******连接以太坊客户端**************/
+    private void connectETHclient() throws IOException {
+        //连接方式1：使用infura 提供的客户端
+        //mainnet https://mainnet.infura.io/v3/2b86c426683f4a6095fd175fe931d799
+        web3j = Web3j.build(new HttpService("https://mainnet.infura.io/v3/9eb78bae70c34116a2b28db3fdb96dd0"));// TODO: 2018/4/10 节点更改为自己的或者主网
+        //连接方式2：使用本地客户端
+        //web3j = Web3j.build(new HttpService("127.0.0.1:7545"));
+        //测试是否连接成功
+        String web3ClientVersion = web3j.web3ClientVersion().send().getWeb3ClientVersion();
+        log.info("version=" + web3ClientVersion);
+    }
+
+
+    /**
+     * *****
+     * *******创建一个智能合约******
+     * <p>
+     * nonce 当nonce太小，交易会被直接拒绝。
+     * 当nonce太大，交易会一直处于队列之中，这也就是导致我们上面描述的问题的原因；
+     * 当发送一个比较大的nonce值，然后补齐开始nonce到那个值之间的nonce，那么交易依旧可以被执行。
+     * 当交易处于queue中时停止geth客户端，那么交易queue中的交易会被清除掉。
+     * 解决这个问题的方法是自己维护Nonce，同一个账户连续发送俩笔交易时，通过递增Nonce来防止Nonce重复。
+     ***********/
+    public void createContract(String from) throws InterruptedException, ExecutionException {
+// using a raw transaction
+        if (web3j == null) return;
+
+
+        EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount(
+                from, DefaultBlockParameterName.LATEST).sendAsync().get();
+        BigInteger nonce = ethGetTransactionCount.getTransactionCount();
+
+        //Generate a smart contract address. This enables you to identify what address a
+        //     *  smart contract will be deployed to on the network.
+        String result = ContractUtils.generateContractAddress(from, nonce);
+
+        log.info("createContract web3j" + result);
+    }
+
+
+    public void createContract(String from, BigInteger ethAmount) throws InterruptedException, ExecutionException, IOException {
+        if (web3j == null) return;
+
+        EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount(
+                from, DefaultBlockParameterName.LATEST).sendAsync().get();
+        BigInteger nonce = ethGetTransactionCount.getTransactionCount();
+        // using a raw transaction
+        BigInteger gasPrice = web3j.ethGasPrice().send().getGasPrice();
+        //BigInteger gasLimit =getTransactionGasLimit(web3j,null);//每笔交易的gas最少21000
+        BigInteger gasLimit = new BigInteger(String.valueOf(21000L));//每笔交易的gas最少21000
+        RawTransaction rawTransaction = RawTransaction.createContractTransaction(nonce, gasPrice, gasLimit, ethAmount, "0x <compiled smart contract code>");
+// send...
+
+    }
+
+    public static BigInteger getTransactionGasLimit(Web3j web3j, org.web3j.protocol.core.methods.request.Transaction transaction) {
+        try {
+            EthEstimateGas ethEstimateGas = web3j.ethEstimateGas(transaction).send();
+            if (ethEstimateGas.hasError()) {
+                throw new RuntimeException(ethEstimateGas.getError().getMessage());
+            }
+            return ethEstimateGas.getAmountUsed();
+        } catch (IOException e) {
+            throw new RuntimeException("net error");
+        }
+    }
+
+    /************获取账户ETH余额***************/
+    public static BigDecimal getBalance(Web3j web3j, String address) {
+        try {
+            EthGetBalance ethGetBalance = web3j.ethGetBalance(address, DefaultBlockParameterName.LATEST).send();
+            return Convert.fromWei(new BigDecimal(ethGetBalance.getBalance()), Convert.Unit.ETHER);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /*********获取交易Hash获取合约地址***********/
+    private String getContractAddress(String transactionHash) throws IOException {
+        // get contract address
+        EthGetTransactionReceipt transactionReceipt =
+                web3j.ethGetTransactionReceipt(transactionHash).send();
+
+        if (transactionReceipt.getTransactionReceipt().isPresent()) {
+            return transactionReceipt.getResult().getContractAddress();
+        }
+        return null;
+    }
+
+
+
+    /*************与智能合约交易***************/
+//    private void transWithContract(){
+//        Collection.Function function = new Collection.Function<>(
+//                "functionName",  // function we're calling
+//                Arrays.asList(new Type(value), ...),  // Parameters to pass as Solidity Types
+//        Arrays.asList(new TypeReference<Type>() {}, ...));
+//
+//        String encodedFunction = FunctionEncoder.encode(function)
+//        Transaction transaction = Transaction.createFunctionCallTransaction(
+//                <from>, <gasPrice>, <gasLimit>, contractAddress, <funds>, encodedFunction);
+//
+//        org.web3j.protocol.core.methods.response.EthSendTransaction transactionResponse =
+//                web3j.ethSendTransaction(transaction).sendAsync().get();
+//
+//        String transactionHash = transactionResponse.getTransactionHash();
+//
+//// wait for response using EthGetTransactionReceipt...
+//    }
 
 }
--- src/main/java/com/we3j/demo/test/WalletDemo.java
+++ src/main/java/com/we3j/demo/test/WalletDemo.java
@@ -22,12 +22,13 @@ import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
 import java.util.Scanner;
 
 /**
  * @Author jambestwick
  * @create 2021/11/30 0030  0:09
- * @email jambestwick@126.com
+ * @email jambestwick1988@gmail.com
  */
 
 public class WalletDemo {
@@ -45,10 +46,9 @@ public class WalletDemo {
 
     private void run1() throws Exception {
         log.info("hello eth,hello web3j");
-        conectETHclient();//连接以太坊客户端
+        connectETHclient();//连接以太坊客户端
         //creatAccount();//创建冷钱包
         String privateKey = "";
-        String wechat = "";
         String keyStoreDir = WalletUtils.getDefaultKeyDirectory();
         System.out.println("生成keyStore文件的默认目录：" + keyStoreDir);
         //通过密码及keystore目录生成钱包
@@ -58,65 +58,8 @@ public class WalletDemo {
 //        //12个单词的助记词
 //        System.out.println(wallet.getMnemonic());
 
-        System.out.println("离线工具，放心使用,源码在github.com/jambestwick");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入作者的微信号，方能使用:");
-        if (scanner.hasNext()) {
-            wechat = scanner.nextLine();
-        }
-        if (wechat == null) {
-            System.out.println("对不起，你不认识作者！");
-            return;
-        }
-        if (!wechat.equals("jambestwick")) {
-            System.out.println("对不起，你不认识作者！");
-            return;
-        }
-        System.out.println("离线工具，放心使用,输入私钥/offLine tools  safe, input privateKey：");
-
-        privateKey = scanner.nextLine();
-
-        loadWalletByPrimaryKeyPassword(privateKey);//加载钱包
+        loadWalletByPrivateKey(privateKey);//加载钱包
         //先获取Sign码
-
-        while (true) {
-            String monaMessage = RequestUtil.requestGet(Constants.GET_MESSAGE);
-            if (monaMessage == null) {
-                //未获取到Message
-                continue;
-            }
-            //不断尝试随机生成code码
-            String signMessage = sign(tempAddress, monaMessage);
-            if (signMessage == null) {
-                continue;
-            }
-            //随机生成邀请码
-            String inviteCode = RandomUtil.randomNumDigest();
-            String response = RequestUtil.requestPost(Constants.POST_INVITE_CODE
-                    , BuildInviteCodeRequest.buildInviteParam(tempAddress, inviteCode, signMessage)
-                    , BuildInviteCodeRequest.buildHead("application/json, text/plain, */*"
-                            , "gzip, deflate, br"
-                            , "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7"
-                            , "application/json"
-                            , "https://monaconft.io"
-                            , "https://monaconft.io/"
-                            , "\"Chromium\";v=\"94\", \"Google Chrome\";v=\"94\", \";Not A Brand\";v=\"99\""
-                            , "?1"
-                            , "Android"
-                            , "empty"
-                            , "cors"
-                            , "same-site"
-                            , ""
-                            , "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Mobile Safari/537.36")
-            );
-
-            if (response != null && !response.contains("wrong invite code or has been taken")) {//邀请码成功，不继续
-                System.out.println("恭喜你，成功获取到了邀请码，进入Mona:" + response.toLowerCase());
-                System.out.println("Congratulations，Success get the invite code，Mona login");
-
-                break;
-            }
-        }
 
         //getBlanceOf();//查询账户余额
         //transto();//转账到指定地址
@@ -124,10 +67,10 @@ public class WalletDemo {
 
 
     /*******连接以太坊客户端**************/
-    private void conectETHclient() throws IOException {
+    private void connectETHclient() throws IOException {
         //连接方式1：使用infura 提供的客户端
         //mainnet https://mainnet.infura.io/v3/2b86c426683f4a6095fd175fe931d799
-        web3j = Web3j.build(new HttpService("https://mainnet.infura.io/v3/9eb78bae70c34116a2b28db3fdb96dd0"));// TODO: 2018/4/10 token更改为自己的或者主网
+        web3j = Web3j.build(new HttpService("https://mainnet.infura.io/v3/9eb78bae70c34116a2b28db3fdb96dd0"));// TODO: 2018/4/10 节点更改为自己的或者主网
         //连接方式2：使用本地客户端
         //web3j = Web3j.build(new HttpService("127.0.0.1:7545"));
         //测试是否连接成功
@@ -139,20 +82,41 @@ public class WalletDemo {
     /*************创建一个钱包文件**************/
     private void creatAccount() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException, CipherException, IOException {
         String walletFileName0 = "";//文件名
-        String walletFilePath0 = "/Users/jam/MyGitHub/z_wallet_temp";//钱包文件保持路径，请替换位自己的某文件夹路径
+        String walletFilePath0 = "D:\\JavaWorkSpace\\we3jdemo\\src\\main\\java\\com\\we3j\\demo";//钱包文件保持路径，请替换位自己的某文件夹路径
+        //String walletFilePath0 = "/Users/jam/MyGitHub/z_wallet_temp";//钱包文件保持路径，请替换位自己的某文件夹路径
 
-        walletFileName0 = WalletUtils.generateNewWalletFile("123456", new File(walletFilePath0), false);
-        //WalletUtils.generateFullNewWalletFile("password1",new File(walleFilePath1));
-        //WalletUtils.generateLightNewWalletFile("password2",new File(walleFilePath2));
+        walletFileName0 = WalletUtils.generateNewWalletFile("123456", new File(walletFilePath0), false);//根据密码生成钱包到地址路径
+        //walletFileName0 = WalletUtils.generateLightNewWalletFile("password1", new File(walletFilePath0));//
+        //WalletUtils.generateLightNewWalletFile("password2",new File(walletFilePath0));
+        //WalletUtils.generateBip39Wallet("password2",new File(walletFilePath0));//助记词钱包生成
         log.info("walletName: " + walletFileName0);
     }
 
-    /********加载钱包文件**********/
-    private void loadWallet() throws IOException, CipherException {
+//
+//    /**
+//     * 创建bip39钱包
+//     *
+//     * @param password 钱包密码
+//     * @return CommonWallet
+//     * @throws CipherException e
+//     */
+//    public Bip39Wallet2 generateBip39Wallet(String password) throws CipherException {
+//        byte[] initialEntropy = new byte[16];
+//        SecureRandom.getInstance().nextBytes(initialEntropy);
+//        String mnemonic = MnemonicUtils.generateMnemonic(initialEntropy);
+//        byte[] seed = MnemonicUtils.generateSeed(mnemonic, password);
+//        ECKeyPair ecKeyPair = ECKeyPair.create(Hash.sha256(seed));
+//        WalletFile walletFile = WalletUtils.generateWalletFile(password, ecKeyPair, false,false);
+//        return new Bip39Wallet2(walletFile.getAddress(), mnemonic, password, JSON.toJSONString(walletFile), ecKeyPair.getPrivateKey(), ecKeyPair.getPublicKey());
+//    }
+
+
+    /********根据密码，钱包文件加载钱包文件**********/
+    private void loadWallet(String password, String walletFilePath) throws IOException, CipherException {
         // FIXME: 2018/4/15 替换为自己的钱包路径
-        String walleFilePath = "/Users/yepeng/MyGitHub/z_wallet_temp/UTC--2018-04-10T02-51-24.815000000Z--12571f46ec3f81f7ebe79112be5883194d683787.json";
-        String passWord = "123456";
-        credentials = WalletUtils.loadCredentials(passWord, walleFilePath);
+        //String walletFilePath = "/Users/jambestwick/MyGitHub/z_wallet_temp/UTC--2018-04-10T02-51-24.815000000Z--12571f46ec3f81f7ebe79112be5883194d683787.json";
+        //String password = "123456";
+        credentials = WalletUtils.loadCredentials(password, walletFilePath);
         String address = credentials.getAddress();
         BigInteger publicKey = credentials.getEcKeyPair().getPublicKey();
         BigInteger privateKey = credentials.getEcKeyPair().getPrivateKey();
@@ -162,13 +126,10 @@ public class WalletDemo {
         log.info("private key=" + privateKey);
     }
 
-    /**
-     * 根据助记词连接
-     **/
 
-    private void loadWalletByPrimaryKeyPassword(String privateKeys) throws IOException, CipherException {
-
-        credentials = Credentials.create(privateKeys);
+    /********根据助记词加载钱包 **********/
+    private void loadWalletByPrimaryKeyPassword(String password, String mnemonic) {
+        WalletUtils.loadBip39Credentials(password, mnemonic);//no need password
 //        credentials = WalletUtils.loadBip39Credentials(password,
 //                keyWords);//"cherry type collect echo derive shy balcony dog concert picture kid february"
         String address = credentials.getAddress();
@@ -182,12 +143,31 @@ public class WalletDemo {
 
     }
 
+    /********根据私钥加载钱包 **********/
+    private void loadWalletByPrivateKey(String inputPrivateKey) {
+        credentials = Credentials.create(inputPrivateKey);
+        String address = credentials.getAddress();
+        BigInteger publicKey = credentials.getEcKeyPair().getPublicKey();
+        BigInteger privateKey = credentials.getEcKeyPair().getPrivateKey();
+
+        log.info("address=" + address);
+        log.info("public key=" + publicKey);
+        log.info("private key=" + privateKey);
+        tempAddress = address;
+
+    }
+
+
     /****************交易*****************/
     private void transto() throws Exception {
         if (web3j == null) return;
         if (credentials == null) return;
         //开始发送0.01 =eth到指定地址
         String address_to = "0x41F1dcbC0794BAD5e94c6881E7c04e4F98908a87";
+        //转账时最好判断地址的有效性,谨防由于错行少字，金额丢失
+        if (!WalletUtils.isValidAddress(address_to)) {
+            return;
+        }
         TransactionReceipt send = Transfer.sendFunds(web3j, credentials, address_to, BigDecimal.ONE, Convert.Unit.FINNEY).send();
 
         log.info("Transaction complete:");
@@ -198,14 +178,14 @@ public class WalletDemo {
         log.info("status: " + send.getStatus());
     }
 
-    /********签名**********/
-    private String sign(String address, String message) throws Exception {
+    /********对指定信息签名**********/
+    private String sign(String message) throws Exception {
         if (web3j == null) return null;
         if (credentials == null) return null;
 
         byte[] hexMessage = Hash.sha3(message.getBytes());
 
-        Sign.SignatureData signatureData = Sign.signMessage(hexMessage, credentials.getEcKeyPair());
+        Sign.SignatureData signatureData = Sign.signMessage(hexMessage, credentials.getEcKeyPair());//例如metamask 登录的签名信息
         //EthSign ethSign = web3j.ethSign(address, Hash.sha3(message)).send();
 
         String resultSignature = signatureData.toString();
@@ -225,6 +205,11 @@ public class WalletDemo {
         log.info(blanceETH);
     }
 
+
+    /***********查询指定地址的余额***********/
+
+
+
     /**
      * 初始化admin级别操作的对象
      *
@@ -233,6 +218,7 @@ public class WalletDemo {
     private Admin initAdmin() {
         return Admin.build(getService());
     }
+
 
     private HttpService getService() {
         if (httpService == null) {
